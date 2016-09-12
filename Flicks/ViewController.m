e0024328dc5a77374d2026066757d3d0f6be42be
Yash Kshirsagar <yashk@yahoo-inc.com>
2016-09-12 14:04:42 -0700

    Initial Commit

---
+++ Flicks/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Flicks
+//
+//  Created by Yash Kshirsagar on 9/12/16.
+//  Copyright © 2016 Yash Kshirsagar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
